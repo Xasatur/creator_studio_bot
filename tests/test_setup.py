537f5eaaa88849e2783a84e0b5ec4c28bda1aad2
Leonardo Cesconi <leonardo.cesconi@stud.hslu.ch>
2025-05-31 12:24:12 +0200

    Fixed Test_setup, enviroment is set up correctly.

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,13 +1,13 @@
 # test_setup.py
+import importlib.metadata
 import discord
 import sqlalchemy
-import dotenv
 import aiohttp
 import PIL
 
 print("Discord.py version:", discord.__version__)
 print("SQLAlchemy version:", sqlalchemy.__version__)
-print("python-dotenv version:", dotenv.__version__)
+print("python-dotenv version:", importlib.metadata.version("python-dotenv"))
 print("aiohttp version:", aiohttp.__version__)
 print("Pillow version:", PIL.__version__)
 print("\nAll required packages are installed correctly!")
